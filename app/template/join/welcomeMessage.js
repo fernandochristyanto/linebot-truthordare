1ed4bc563f794e1aa76cfbe0a1a82456aad4f27b
Fernando Christyanto <christyantofernando@gmail.com>
2018-10-04 14:24:39 +0700

    Add bot_name to env

--- app/template/join/welcomeMessage.js
+++ app/template/join/welcomeMessage.js
@@ -2,7 +2,5 @@ const { MESSAGE_TYPE } = require('../../data/messagingAPI/messageType')
 
 exports.welcomeMessage = {
   type: MESSAGE_TYPE.TEXT,
-  text: `Hai Fellas,\n
-          Thank you for adding us.\n
-          We hope you enjoy our bot.`
+  text: `Hai Fellas,\nThank you for adding us.\nWe hope you enjoy our bot.`
 }
--- config.js
+++ config.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const config = {
   channelAccessToken: process.env.CHANNEL_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
-  mongodbUri: process.env.MONGODB_URI
+  mongodbUri: process.env.MONGODB_URI,
+  botName: process.env.BOT_NAME
 }
 
 module.exports = config;

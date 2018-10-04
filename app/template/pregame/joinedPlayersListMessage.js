e193edd0b24484d1a2d3120bdc380187d71195f6
Fernando Christyanto <christyantofernando@gmail.com>
2018-10-04 20:44:40 +0700

    Fix no player joined the game typo

--- app/template/pregame/joinedPlayersListMessage.js
+++ app/template/pregame/joinedPlayersListMessage.js
@@ -8,8 +8,8 @@ exports.joinedPlayerListMessage = (joinedPlayers) => {
 }
 
 function mapJoinedPlayersToMessage(joinedPlayers) {
-  if(joinedPlayers.length == 0)
-    return "No player has joined the game :(F"
+  if (joinedPlayers.length == 0)
+    return "No player has joined the game :("
 
   let text = 'Here is the list of players who are brave and honest :\n'
   joinedPlayers.forEach((player) => {
